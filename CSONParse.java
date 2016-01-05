16046dc3e622965084bc595a70e15daf3d72114c
Patrick Gallagher <patrickj@cpgallagher.com>
2016-01-05 00:27:45 -0600

    There we have it! Now add in arguments and that's a release!

--- CSONParse.java
+++ CSONParse.java
@@ -2,7 +2,37 @@ import java.io.*;
 import java.util.ArrayList;
 public class CSONParse {
 
-  //public static void generateCSON(ArrayList data)
+  public static void generateCSON(ArrayList<Snippet> data){
+    String fileName = "racketSnippets.cson";
+    
+    try {
+     FileWriter fw = new FileWriter(fileName);
+     
+     BufferedWriter bw = new BufferedWriter(fw);
+     
+     bw.write("'.source.racket':");
+     bw.newLine();
+      for (Snippet snip : data){
+       bw.write("\t'" + snip.function + " function':");
+       bw.newLine();
+       bw.write("\t\t'prefix': '" + snip.function + "'");
+       bw.newLine();
+       bw.write("\t\t'body': \"(" + snip.function + ")\"");
+       bw.newLine();
+      }
+     
+     
+     bw.close();
+     
+    }catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+    
+  }
   
   public static void readFile(String filename)throws FileNotFoundException, IOException{
     boolean nextLine = false;
@@ -23,20 +53,28 @@ public class CSONParse {
             nextLine = false;
           }
        }
-       System.out.println(functions);
+       
        for(String item : functions){
          String[] splitItem;
          splitItem = item.split(":", 2);
-         System.out.println(item);
+         
          
          splitFunctions.add(splitItem);
        }
-       System.out.println("LIST OF FUNCTIONS:");
+       
        for(String[] item : splitFunctions){
          Snippet snip = new Snippet("","");
-         snip.function = item[0].replace(" ", ""); 
-         
+         snip.function = item[0].replace(" ", ""); 
+         String tempArg = item[1];
+         tempArg = tempArg.substring(2);
+         snip.args = tempArg.split("-> [^\\%]",2)[0];
+         snippets.add(snip);
        }
+       for(Snippet item : snippets){
+         System.out.println(item.function + " which has the syntax " + item.args);
+         //System.out.println(item.args);
+       }
+       generateCSON(snippets);
      }
   }
 
