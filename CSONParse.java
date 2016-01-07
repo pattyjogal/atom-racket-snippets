74583a7b36fc7feeee276c2ca07f3b30af445269
Patrick Gallagher <patrickj@cpgallagher.com>
2016-01-06 22:30:11 -0600

    There we go! CSONParse.java now renders with arguments as well!

--- CSONParse.java
+++ CSONParse.java
@@ -4,26 +4,35 @@ public class CSONParse {
 
   public static void generateCSON(ArrayList<Snippet> data){
     String fileName = "racketSnippets.cson";
-    
+    int argsl;
     try {
      FileWriter fw = new FileWriter(fileName);
-     
+
      BufferedWriter bw = new BufferedWriter(fw);
-     
+
      bw.write("'.source.racket':");
      bw.newLine();
       for (Snippet snip : data){
+       argsl = snip.args.length;
        bw.write("\t'" + snip.function + " function':");
        bw.newLine();
        bw.write("\t\t'prefix': '" + snip.function + "'");
        bw.newLine();
-       bw.write("\t\t'body': \"(" + snip.function + ")\"");
+       bw.write("\t\t'body': \"(" + snip.function);
+       for (int i = 0; i < argsl; i++){
+         if (i > 0){
+           bw.write(" ");
+         }
+         bw.write("${" + Integer.toString(i + 1) + ": " + snip.args[i] + "}");
+       }
+       bw.write("${" + Integer.toString(argsl + 1) + "})${"  + Integer.toString(argsl + 2) +"}\"");
+
        bw.newLine();
       }
-     
-     
+
+
      bw.close();
-     
+
     }catch(IOException ex) {
             System.out.println(
                 "Error writing to file '"
@@ -31,9 +40,9 @@ public class CSONParse {
             // Or we could just do this:
             // ex.printStackTrace();
         }
-    
+
   }
-  
+
   public static void readFile(String filename)throws FileNotFoundException, IOException{
     boolean nextLine = false;
     ArrayList<String> functions = new ArrayList<String>();
@@ -41,42 +50,45 @@ public class CSONParse {
     ArrayList<Snippet> snippets = new ArrayList<Snippet>();
      try (BufferedReader br = new BufferedReader(new FileReader(filename))){
        for (String line; (line = br.readLine()) != null; ) {
-         
+
           if (line.contains("function")){
             nextLine = true;
-            
+
           }else if (nextLine) {
             nextLine = false;
-            line = line.replace("Â", "");
+            line = line.replace("ï¿½", "");
             functions.add(line);
           }else{
             nextLine = false;
           }
        }
-       
+
        for(String item : functions){
          String[] splitItem;
          splitItem = item.split(":", 2);
-         
-         
+
+
          splitFunctions.add(splitItem);
        }
-       
+
        for(String[] item : splitFunctions){
-         Snippet snip = new Snippet("","");
-         snip.function = item[0].replace(" ", ""); 
+         Snippet snip = new Snippet("",new String[]{""});
+         snip.function = item[0].replace("ï¿½", "");
          String tempArg = item[1];
          tempArg = tempArg.substring(2);
-         snip.args = tempArg.split("-> [^\\%]",2)[0];
+         snip.args = new String[]{tempArg.split("->ï¿½[^\\%]",2)[0]};
+         snip.args = snip.argParse();
+        // System.out.println(snip.argParse()[0]);
          snippets.add(snip);
        }
        for(Snippet item : snippets){
-         System.out.println(item.function + " which has the syntax " + item.args);
+         //System.out.println(item.args[0]);
          //System.out.println(item.args);
        }
        generateCSON(snippets);
      }
   }
+
 
   public static void main(String[] args) throws FileNotFoundException, IOException{
     String path = "C:\\Program Files\\Racket\\doc\\deinprogramm\\blueboxes.txt";
--- Snippet.java
+++ Snippet.java
@@ -3,10 +3,32 @@
  */
 public class Snippet {
   String function;
-  String args;
-  public Snippet(String a, String b){
+  String[] args;
+  boolean shouldSplit;
+  public Snippet(String a, String[] b){
     function = a;
     args = b;
+  }
+  public String[] argParse(){
+   String[] argString = this.args;
+   shouldSplit = !argString[0].contains("(");
+   String[] splitArgs = argString;
+   if (shouldSplit){
+     splitArgs = argString[0].split(" ");
+     System.out.println("---------");
+     System.out.println(splitArgs.length);
+    
+     for (String item : splitArgs){
+       System.out.println(item);
+     }
+   }
+    for (String str : splitArgs){
+      if (str.contains("...") && shouldSplit){
+        str = "";
+      }
+        
+    }
+    return splitArgs;
   }
   
 }
